9b6250ca96d120c234aa039c436e1c45ea59411e
signalwerk <sh@signalwerk.ch>
2025-09-28 11:20:29 +0200

    ADD: neo setup

---
+++ neo/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Scarf</h1>
+      <p>Basic pattern</p>
+    </>
+  );
+}
+
+export default App;
